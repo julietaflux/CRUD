54444dc78b61612c0d18cfb570b9a2e07fe1a630
julietaflux <julietaflux@gmail.com>
2020-12-31 18:47:51 -0300

    refactors again

---
+++ react-frontend/src/components/HeaderComponent.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HeaderComponent = () => (
+  <div>
+    <header>
+      <nav className="navbar navbar-expand-md navbar-light bg-light">
+        <div>
+          <a className="navbar-brand">Product Management App</a>
+        </div>
+      </nav>
+    </header>
+  </div>
+);
+
+export default HeaderComponent;
